abb4312e19b0e1a01628668750d19efa2b6a3a3c
Jacob Anter <anter.jacob@gmail.com>
2021-01-22 16:31:55 +0100

    Adding utility file for comfortable access to test structures

---
+++ tests/util.py
@@ -0,0 +1,8 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+from os.path import join, dirname, realpath
+
+def struc_for_test_dir(file):
+    return join(dirname(realpath(__file__)), "data", file)
