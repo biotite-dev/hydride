ddb6658bcdb994a325f2d701ae7a7fedfa0b5805
Jacob Anter <anter.jacob@gmail.com>
2021-01-25 15:33:21 +0100

    Correcting package name in the licence

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,4 +1,4 @@
-# This source code is part of the Biotite package and is distributed
+# This source code is part of the Hydride package and is distributed
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
