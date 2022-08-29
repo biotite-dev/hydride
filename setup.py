a21db7ddabf0688c60cb33e21cd07db93e86396b
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-08-29 11:16:32 +0200

    Fix installation of source distribution

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 import warnings
 import re
-from os.path import join, abspath, dirname, normpath
+from os.path import join, abspath, dirname, normpath, isfile
 import fnmatch
 import os
 import pickle
@@ -123,48 +123,54 @@ def get_protonation_variants():
     return molecules
 
 # Compile fragment library
-std_fragment_library = FragmentLibrary()
-# Add protonation variants at first because the bond lengths for neutral
-# fragments might variate slightly
-for mol in get_protonation_variants():
-    std_fragment_library.add_molecule(mol)
-mol_names = list(_res_names.keys()) + PROMINENT_MOLECULES
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    for i, mol_name in enumerate(mol_names):
-        if not i % 100:
-            print(
-                f"Compiling fragment library... ({i}/{len(mol_names)})",
-                end="\r"
-            )
-        try:
-            mol = info.residue(mol_name)
-        except KeyError:
-            continue
+fragment_file_path = join("src", "hydride", "fragments.pickle")
+if not isfile(fragment_file_path):
+    std_fragment_library = FragmentLibrary()
+    # Add protonation variants at first because the bond lengths
+    # for neutral fragments might variate slightly
+    for mol in get_protonation_variants():
         std_fragment_library.add_molecule(mol)
-print("Compiling fragment library... Done" + " " * 20)
-with open(join("src", "hydride", "fragments.pickle"), "wb") as fragments_file:
-    pickle.dump(std_fragment_library._frag_dict, fragments_file)
+    mol_names = list(_res_names.keys()) + PROMINENT_MOLECULES
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        for i, mol_name in enumerate(mol_names):
+            if not i % 100:
+                print(
+                    f"Compiling fragment library... ({i}/{len(mol_names)})",
+                    end="\r"
+                )
+            try:
+                mol = info.residue(mol_name)
+            except KeyError:
+                continue
+            std_fragment_library.add_molecule(mol)
+    print("Compiling fragment library... Done" + " " * 20)
+    with open(fragment_file_path, "wb") as fragments_file:
+        pickle.dump(std_fragment_library._frag_dict, fragments_file)
+
 
 # Compile atom name library
-std_name_library = AtomNameLibrary()
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    # To reduce the size of the atom name library, only atom names
-    # from prominent molecules are stored
-    for i, mol_name in enumerate(PROMINENT_MOLECULES):
-        print(
-            f"Compiling atom name library... ({i+1}/{len(PROMINENT_MOLECULES)})",
-            end="\r"
-        )
-        try:
-            mol = info.residue(mol_name)
-        except KeyError:
-            continue
-        std_name_library.add_molecule(mol)
-print("Compiling atom name library... Done" + " " * 20)
-with open(join("src", "hydride", "names.pickle"), "wb") as names_file:
-    pickle.dump(std_name_library._name_dict, names_file)
+names_file_path = join("src", "hydride", "names.pickle")
+if not isfile(names_file_path):
+    std_name_library = AtomNameLibrary()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # To reduce the size of the atom name library, only atom names
+        # from prominent molecules are stored
+        for i, mol_name in enumerate(PROMINENT_MOLECULES):
+            print(
+                f"Compiling atom name library... "
+                f"({i+1}/{len(PROMINENT_MOLECULES)})",
+                end="\r"
+            )
+            try:
+                mol = info.residue(mol_name)
+            except KeyError:
+                continue
+            std_name_library.add_molecule(mol)
+    print("Compiling atom name library... Done" + " " * 20)
+    with open(names_file_path, "wb") as names_file:
+        pickle.dump(std_name_library._name_dict, names_file)
 
 
 
